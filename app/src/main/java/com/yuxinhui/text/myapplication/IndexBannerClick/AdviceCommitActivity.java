2330db7f770aa44127108a2385c7370e78e058b8
yuxinhui <1184463211@qq.com>
2016-06-30 10:27:20 +0000

    关于我们的三个Activity

---
+++ app/src/main/java/com/yuxinhui/text/myapplication/IndexBannerClick/AdviceCommitActivity.java
@@ -0,0 +1,15 @@
+package com.yuxinhui.text.myapplication.IndexBannerClick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yuxinhui.text.myapplication.R;
+
+public class AdviceCommitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advice_commit);
+    }
+}
---
+++ app/src/main/java/com/yuxinhui/text/myapplication/IndexBannerClick/VersionINfoActivity.java
@@ -0,0 +1,15 @@
+package com.yuxinhui.text.myapplication.IndexBannerClick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yuxinhui.text.myapplication.R;
+
+public class VersionINfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_version_info);
+    }
+}
