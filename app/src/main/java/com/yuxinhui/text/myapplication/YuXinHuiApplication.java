d87f37e4f856b0950d83f113780832ef8ec5b9e7
yuxinhui <1184463211@qq.com>
2016-07-04 10:32:16 +0000

    添加Application去存放全局变量

---
+++ app/src/main/java/com/yuxinhui/text/myapplication/YuXinHuiApplication.java
@@ -0,0 +1,73 @@
+package com.yuxinhui.text.myapplication;
+
+import android.app.Application;
+
+import com.yuxinhui.text.myapplication.Utils.User;
+
+/**
+ * 用于存放全局变量的类
+ * Created by Administrator on 2016/7/4.
+ */
+
+public class YuXinHuiApplication extends Application {
+    private boolean isLogin=false;
+    private boolean isRing = true;
+    private boolean isVirbate = true;
+    private boolean isOpenMiandarao = false;
+    private User user;
+    private static YuXinHuiApplication instace;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instace = this;
+    }
+
+    public boolean isLogin() {
+        return isLogin;
+    }
+
+    public void setLogin(boolean login) {
+        isLogin = login;
+    }
+
+    public boolean isRing() {
+        return isRing;
+    }
+
+    public void setRing(boolean ring) {
+        isRing = ring;
+    }
+
+    public boolean isVirbate() {
+        return isVirbate;
+    }
+
+    public void setVirbate(boolean virbate) {
+        isVirbate = virbate;
+    }
+
+    public boolean isOpenMiandarao() {
+        return isOpenMiandarao;
+    }
+
+    public void setOpenMiandarao(boolean openMiandarao) {
+        isOpenMiandarao = openMiandarao;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public static YuXinHuiApplication getInstace() {
+        return instace;
+    }
+
+    public static void setInstace(YuXinHuiApplication instace) {
+        YuXinHuiApplication.instace = instace;
+    }
+}
