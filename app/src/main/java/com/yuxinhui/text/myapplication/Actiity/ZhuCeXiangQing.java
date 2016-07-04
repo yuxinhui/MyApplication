9d348e57c10102cedc1042b7d6a9caa353207eee
yuxinhui <1184463211@qq.com>
2016-07-04 10:13:21 +0000

    对于注册的修改

--- app/src/main/java/com/yuxinhui/text/myapplication/Actiity/ZhuCeXiangQing.java
+++ app/src/main/java/com/yuxinhui/text/myapplication/Actiity/ZhuCeXiangQing.java
@@ -1,17 +1,134 @@
 package com.yuxinhui.text.myapplication.Actiity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 import com.yuxinhui.text.myapplication.R;
+import com.yuxinhui.text.myapplication.Utils.User;
+import com.yuxinhui.text.myapplication.YuXinHuiApplication;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/6/2.
+ * 提交个人信息的类
  */
 public class ZhuCeXiangQing extends AppCompatActivity {
+    Zhuce zhuce;
+    EditText metUserName,metNick,metQQ,metSex,metFactor;
+    TextView mtvCommit;
+    User user;
+    String url = "http://114.55.98.142/user/perfectInfo";
+    String nick,userName,QQ, gendar,telephone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zhucexiangqing);
+        zhuce = new Zhuce();
+        zhuce.finish();
+        telephone = YuXinHuiApplication.getInstace().getUser().getTelephone();
+        initView();
+        initData();
+        setOnClickListener();
     }
+
+    //将数据封装到User类中
+    private void initData() {
+        nick = metNick.getText().toString();
+        userName = metUserName.getText().toString();
+        QQ = metQQ.getText().toString();
+        gendar = metSex.getText().toString();
+    }
+
+    private void setOnClickListener() {
+        mtvCommit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(nick==null||nick==""){
+                    metNick.setError("昵称不能为空");
+                    metNick.requestFocus();
+                    return;
+                }
+                if(userName==null||userName==""){
+                    metUserName.setError("用户名不能为空");
+                    metUserName.requestFocus();
+                    return;
+                }
+                if (QQ == null || QQ == "") {
+                    metQQ.setError("QQ不能为空");
+                    metQQ.requestFocus();
+                    return;
+                }
+                if(gendar==null||gendar==""){
+                    gendar="男";
+                }
+                user = new User();
+                user.setNickname(nick);
+                user.setUsername(userName);
+                user.setQq(QQ);
+                user.setGender(gendar);
+                user.setTelephone(telephone);
+                updateData();
+            }
+        });
+    }
+
+    public void showTip(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(ZhuCeXiangQing.this,"上传失败请检查网络设置",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    //发送Post请求去完善个人信息
+    private void updateData() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                showTip();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Gson gson = new Gson();
+                String s = gson.toJson(user);
+                Map<String, String> params = new HashMap<>();
+                params.put("user",s);
+                return params;
+            }
+        };
+        queue.add(request);
+    }
+
+    private void initView() {
+        metUserName = (EditText) findViewById(R.id.et_username);
+        metNick = (EditText) findViewById(R.id.et_nick);
+        metQQ = (EditText) findViewById(R.id.et_qq);
+        metSex = (EditText) findViewById(R.id.et_sex);
+        mtvCommit = (TextView) findViewById(R.id.userinfo_commit);
+    }
+
 }
--- app/src/main/java/com/yuxinhui/text/myapplication/Actiity/Zhuce.java
+++ app/src/main/java/com/yuxinhui/text/myapplication/Actiity/Zhuce.java
@@ -6,9 +6,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.gensee.utils.StringUtil;
+import com.google.gson.Gson;
 import com.yuxinhui.text.myapplication.MainActivity;
 import com.yuxinhui.text.myapplication.R;
+import com.yuxinhui.text.myapplication.Utils.DialogUtils;
+import com.yuxinhui.text.myapplication.Utils.Message;
+import com.yuxinhui.text.myapplication.Utils.User;
+import com.yuxinhui.text.myapplication.YuXinHuiApplication;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/6/1.
@@ -21,30 +38,43 @@ public class Zhuce extends AppCompatActivity {
     private EditText zhuce_mingzi_text;
     private EditText zhuce_mima_text;
     private EditText zhuce_writeyanzheng_text;
+    private TextView metVadateCode;
+    String telepone,password,validateCode;
+    User user;
+    String url = "http://114.55.98.142/user/register";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zhuce);
         initView();
+        initData();
         onClick();
     }
+
+    private void initData() {
+        telepone = zhuce_mingzi_text.getText().toString();
+        password = zhuce_mima_text.getText().toString();
+        validateCode = zhuce_writeyanzheng_text.getText().toString();
+    }
+
     //初始化控件
     private void initView() {
         zhuce_return_img= (ImageView) findViewById(R.id.zhuce_return_img);
         zhuce_home_img= (ImageView) findViewById(R.id.zhuce_home_img);
         zhuce_img= (ImageView) findViewById(R.id.zhuce_img);
         zhuce_getyanzheng_img= (ImageView) findViewById(R.id.zhuce_getyanzheng_img);
-        zhuce_mingzi_text= (EditText) findViewById(R.id.zhuce_mingzi_text);
-        zhuce_mima_text= (EditText) findViewById(R.id.zhuce_mima_text);
+        zhuce_mingzi_text= (EditText) findViewById(R.id.et_telephone);
+        zhuce_mima_text= (EditText) findViewById(R.id.et_passwrod);
         zhuce_writeyanzheng_text= (EditText) findViewById(R.id.zhuce_writeyanzheng_text);
+        metVadateCode = (TextView) findViewById(R.id.tv_validate_code);
     }
     //图片响应事件
     private void onClick() {
         zhuce_return_img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent fanhui = new Intent(Zhuce.this, Denglu.class);
-                startActivity(fanhui);
+                Zhuce.this.finish();
                 return;
             }
         });
@@ -59,10 +89,77 @@ public class Zhuce extends AppCompatActivity {
         zhuce_img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent login = new Intent(Zhuce.this,Denglu.class);
-                startActivity(login);
+                if(telepone==null||telepone.equals("")){
+                    zhuce_mingzi_text.setError("手机号不能为空");
+                    zhuce_mingzi_text.requestFocus();
+                    return;
+                }
+                if(StringUtil.isEmpty(password)){
+                    zhuce_mima_text.setError("密码不能为空");
+                    zhuce_mima_text.requestFocus();
+                    return;
+                }
+                if (metVadateCode.getText().toString().equals("重新获取")) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            DialogUtils.createToasdt(Zhuce.this,"验证码已经过期，请重新获取");
+                        }
+                    });
+                    return;
+                }
+                if(StringUtil.isEmpty(validateCode)){
+                    zhuce_writeyanzheng_text.setError("验证码不能为空");
+                    zhuce_writeyanzheng_text.requestFocus();
+                    return;
+                }
+                register();
                 return;
             }
         });
+    }
+
+    public void register(){
+        user = YuXinHuiApplication.getInstace().getUser();
+        user.setTelephone(telepone);
+        user.setPassword(password);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                Gson gson = new Gson();
+                Message message = gson.fromJson(s, Message.class);
+                if(message.getStatus().equals("success")){
+                    Denglu denglu = new Denglu();
+                    boolean isLogin = denglu.login();
+                    if(isLogin){
+                        YuXinHuiApplication.getInstace().setUser(message.getUser());
+                    }
+                    YuXinHuiApplication.getInstace().setLogin(isLogin);
+                }
+                Intent intent = new Intent(Zhuce.this, ZhuCeXiangQing.class);
+                startActivity(intent);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        DialogUtils.createToasdt(Zhuce.this,"请检查网络连接是否正确");
+                    }
+                });
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Gson g = new Gson();
+                Map<String, String> params = new HashMap<>();
+                params.put("user",g.toJson(user));
+                params.put("validateCode", validateCode);
+                return params;
+            }
+        };
+        queue.add(request);
     }
 }
