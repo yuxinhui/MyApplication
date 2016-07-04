e59512313a47192a1b10885bc2e3f59eb00c6422
yuxinhui <1184463211@qq.com>
2016-07-04 10:21:34 +0000

    添加了User实体类

---
+++ app/src/main/java/com/yuxinhui/text/myapplication/Utils/User.java
@@ -0,0 +1,172 @@
+package com.yuxinhui.text.myapplication.Utils;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+
+    private String username;
+
+    private String nickname;
+
+    private String password;
+
+    private String gender;
+
+    private Integer type;
+
+    private String email;
+
+    private String telephone;
+
+    private String qq;
+
+    private String loginIp;
+
+    private String state;
+
+    private Date userTime;
+
+    private String rid;
+
+    private String gid;
+
+    private String level;
+
+	public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender == null ? null : gender.trim();
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone == null ? null : telephone.trim();
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq == null ? null : qq.trim();
+    }
+
+    public String getLoginIp() {
+        return loginIp;
+    }
+
+    public void setLoginIp(String loginIp) {
+        this.loginIp = loginIp == null ? null : loginIp.trim();
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state == null ? null : state.trim();
+    }
+
+    public Date getUserTime() {
+        return userTime;
+    }
+
+    public void setUserTime(Date userTime) {
+        this.userTime = userTime;
+    }
+
+    public String getRid() {
+        return rid;
+    }
+
+    public void setRid(String rid) {
+        this.rid = rid == null ? null : rid.trim();
+    }
+
+    public String getGid() {
+        return gid;
+    }
+
+    public void setGid(String gid) {
+        this.gid = gid == null ? null : gid.trim();
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level == null ? null : level.trim();
+    }
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", nickname="
+				+ nickname + ", password=" + password + ", gender=" + gender
+				+ ", type=" + type + ", email=" + email + ", telephone="
+				+ telephone + ", qq=" + qq + ", loginIp=" + loginIp
+				+ ", state=" + state + ", userTime=" + userTime + ", rid="
+				+ rid + ", gid=" + gid + ", level=" + level + "]";
+	}
+    
+}
