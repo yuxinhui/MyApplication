a0a9474a6492f30527c0ac42074dc137c58803a7
yuxinhui <1184463211@qq.com>
2016-07-04 10:17:00 +0000

    消息设置activity的修改以及版本信息的描述的更新

--- app/src/main/java/com/yuxinhui/text/myapplication/IndexBannerClick/VersionINfoActivity.java
+++ app/src/main/java/com/yuxinhui/text/myapplication/IndexBannerClick/VersionINfoActivity.java
@@ -2,14 +2,33 @@ package com.yuxinhui.text.myapplication.IndexBannerClick;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.yuxinhui.text.myapplication.R;
 
 public class VersionINfoActivity extends AppCompatActivity {
+    ImageView mReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_version_info);
+        initView();
+        setOnClickListener();
+    }
+
+    private void setOnClickListener() {
+        mReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VersionINfoActivity.this.finish();
+            }
+        });
+    }
+
+    private void initView() {
+        mReturn = (ImageView) findViewById(R.id.back_version);
     }
 }
